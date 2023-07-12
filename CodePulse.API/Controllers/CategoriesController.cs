b4fa0bcd2edc45d8a558f684e822824579cde39f
satish saraskar <satishsaraskar@gmail.com>
2023-07-12 13:58:01 +0530

    Building api using asp.net code & create post categories action method

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,73 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CodePulse.API.Controllers
+{
+    //https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        /*   //using inside the mehtod this filed
+private readonly ApplicationDbContext _dbContext;
+
+//i want to use to injected dbcontext class in constaller
+public CategoriesController(ApplicationDbContext dbContext)
+{
+this._dbContext = dbContext;
+}*/
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto req)
+        {
+            //Map DTO to Domain model
+
+            var category = new Category
+            {
+                Name = req.Name,
+                UrlHandle = req.UrlHandle
+            };
+
+
+            //i want to use to injected dbcontext class in constaller
+            //saving the data inside controller that is a bad practice remove 
+            //add it to repository instead  
+
+            /* await _dbContext.Categories.AddAsync(category);
+             await _dbContext.SaveChangesAsync();*/
+
+
+
+            /*          abstraction the implementation to the repository and the controller
+        has no idea how to repository is able to talk and save these changes inside the database*/
+            await categoryRepository.CreateAsync(category);
+
+            //we never expose the domain model
+            // return Ok(category)
+            //Domain model to dto
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
---
+++ CodePulse.API/Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        //only define of the method not an actual implementation
+        Task<Category> CreateAsync (Category category);
+    }
+}
